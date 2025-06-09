df973f7d52c5fe9955eaeb716936aad703d7ca62
AleezinhoCAPUDO <alexandrekk21@gmail.com>
2025-06-09 11:29:16 -0300

    .map foio destruido

---
+++ map/treinoUsandoMap.ts
@@ -0,0 +1,7 @@
+const numeros: number[] = [2,4,6,8,10];
+
+const numerosAoCubo = numeros.map((numeros) => numeros ** 3);
+const numerosMultiplicados = numeros.map((numeros) => numeros * 19);
+const numerosDivididos = numeros.map((numeros) => numeros / 2);
+
+console.log("Os números ao cubo ficam:", numerosAoCubo, "já os números divididos pela metade ficam:", numerosDivididos, "e os númemros vezes 19 ficam:", numerosMultiplicados  );
